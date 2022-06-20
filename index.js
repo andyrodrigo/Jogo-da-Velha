9bd7ebc617d5aca93371e0134a84d6055fedb843
perkles <andyrodrigo2@gmail.com>
2022-06-20 13:42:01 -0300

    Ajuste das caixas

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 
-//Variáveis--------------------------------------------------------
+//Variáveis------------------------------------------------------------------------------
 const espaco = new Array(3)
 espaco[0] = new Array(3)
 espaco[1] = new Array(3)
 espaco[2] = new Array(3)
+//relação com os espaços do html
 espaco[0][0] = document.getElementById('0x0');
 espaco[0][1] = document.getElementById('0x1');
 espaco[0][2] = document.getElementById('0x2');
@@ -13,6 +14,12 @@ espaco[1][2] = document.getElementById('1x2');
 espaco[2][0] = document.getElementById('2x0');
 espaco[2][1] = document.getElementById('2x1');
 espaco[2][2] = document.getElementById('2x2');
+
+//de jogo
+let jogo = false
+
+
+//Funções-------------------------------------------------------------------------------
 
 function inserirXO(e){
     let linha = Number(e.currentTarget.parentNode.rowIndex)
