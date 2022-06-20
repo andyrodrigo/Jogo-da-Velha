75c36cd0447393a1ddba474e79272904a503146d
perkles <andyrodrigo2@gmail.com>
2022-06-20 11:23:35 -0300

    Inicio do Projeto

---
+++ index.js
@@ -0,0 +1,14 @@
+
+//Variáveis--------------------------------------------------------
+const espaco1x1 = document.getElementById('1x1');
+const espaco1x2 = document.getElementById('1x2');
+const espaco1x3 = document.getElementById('1x3');
+const espaco2x1 = document.getElementById('2x1');
+const espaco2x2 = document.getElementById('2x2');
+const espaco2x3 = document.getElementById('2x3');
+const espaco3x1 = document.getElementById('3x1');
+const espaco3x2 = document.getElementById('3x2');
+const espaco3x3 = document.getElementById('3x3');
+
+
+
