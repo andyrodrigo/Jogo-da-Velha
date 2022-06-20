9f3d9393031e3315b834f3934e4b9c3ca37b9add
perkles <andyrodrigo2@gmail.com>
2022-06-20 11:53:05 -0300

    Botões funcionando

--- index.js
+++ index.js
@@ -1,14 +1,41 @@
 
 //Variáveis--------------------------------------------------------
-const espaco1x1 = document.getElementById('1x1');
-const espaco1x2 = document.getElementById('1x2');
-const espaco1x3 = document.getElementById('1x3');
-const espaco2x1 = document.getElementById('2x1');
-const espaco2x2 = document.getElementById('2x2');
-const espaco2x3 = document.getElementById('2x3');
-const espaco3x1 = document.getElementById('3x1');
-const espaco3x2 = document.getElementById('3x2');
-const espaco3x3 = document.getElementById('3x3');
+const espaco = new Array(3)
+espaco[0] = new Array(3)
+espaco[1] = new Array(3)
+espaco[2] = new Array(3)
+espaco[0][0] = document.getElementById('0x0');
+espaco[0][1] = document.getElementById('0x1');
+espaco[0][2] = document.getElementById('0x2');
+espaco[1][0] = document.getElementById('1x0');
+espaco[1][1] = document.getElementById('1x1');
+espaco[1][2] = document.getElementById('1x2');
+espaco[2][0] = document.getElementById('2x0');
+espaco[2][1] = document.getElementById('2x1');
+espaco[2][2] = document.getElementById('2x2');
 
+function inserirXO(e){
+    let linha = Number(e.currentTarget.parentNode.rowIndex)
+    let coluna = Number(e.currentTarget.cellIndex)
+    alert(linha + ' ' + coluna)
+}
 
+//Escutadores---------------------------------------------------------------------------
 
+function escutadores(){
+    espaco[0][0].addEventListener('click', inserirXO)
+    espaco[0][1].addEventListener('click', inserirXO)
+    espaco[0][2].addEventListener('click', inserirXO)
+    espaco[1][0].addEventListener('click', inserirXO)
+    espaco[1][1].addEventListener('click', inserirXO)
+    espaco[1][2].addEventListener('click', inserirXO)
+    espaco[2][0].addEventListener('click', inserirXO)
+    espaco[2][1].addEventListener('click', inserirXO)
+    espaco[2][2].addEventListener('click', inserirXO)
+
+}
+
+//--------------------------------------------------------------------------------------
+
+//inicialização
+window.addEventListener("load", escutadores) //Ativa os escutadores
